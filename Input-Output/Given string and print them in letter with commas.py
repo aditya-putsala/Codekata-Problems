963e7c68affc7b0cdf99056212ca5c0fce7d2923
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-07-09 23:34:13 +0530

    Create Given string and print them in letter with commas.py

---
+++ Input-Output/Given string and print them in letter with commas.py
@@ -0,0 +1,18 @@
+## Write a code to get the input in the given format and print the output in the given format.
+
+## Input Description:
+## A single line contains a string.
+
+## Output Description:
+## Print the characters in a string separated by comma.
+
+## Sample Input :
+## guvi
+## Sample Output :
+## g,u,v,i
+
+n = input()
+new = []
+for i in n:
+    new.append(i)
+print(",".join(new))
