b0e828bfd8f85b2390f2588886c91045c9ab98eb
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-07-12 19:04:24 +0530

    Create find the given year is leap or not.py

---
+++ absolute beginner/find the given year is leap or not.py
@@ -0,0 +1,25 @@
+## Let "A" be a year, write a program to check whether this year is a leap year or not.
+
+## Print "Y" if its a leap year and "N" if its a common year.
+
+## Input Description:
+## A Year is the input in the form of a positive integer.
+
+## Output Description:
+## Print "Y" if its a leap year and "N" if its a common year.
+
+## Sample Input :
+## 2020
+## Sample Output :
+## Y
+
+n = int(input())
+if n % 100 == 0:
+  if n % 400 == 0:
+    print("Y")
+  else:
+    print("N")
+elif n % 4 == 0 and n % 100 != 0:
+  print("Y")
+else:
+  print("N")
