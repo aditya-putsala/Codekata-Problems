e13936a265460bbc2a2f19e8f9d45a65bdb6266c
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-07-12 19:14:45 +0530

    Create print the numbers descending order in vertical manner from the given number.py

---
+++ absolute beginner/print the numbers descending order in vertical manner from the given number.py
@@ -0,0 +1,26 @@
+## Write a code to get an integer N and print the values from N to 1.
+
+## Input Description:
+## A single line contains an integer N.
+
+## Output Description:
+## Print the values from N to 1 in a separate line.
+
+## Sample Input :
+## 10
+## Sample Output :
+## 10
+## 9
+## 8
+## 7
+## 6
+## 5
+## 4
+## 3
+## 2
+## 1
+
+n = int(input())
+while n>0:
+  print(n)
+  n-=1
