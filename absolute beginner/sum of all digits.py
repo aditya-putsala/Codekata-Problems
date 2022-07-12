b26aea5afd9036a27b829a8aa34ced5341d8323f
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-07-12 19:07:54 +0530

    Create sum of all digits.py

---
+++ absolute beginner/sum of all digits.py
@@ -0,0 +1,18 @@
+## Write a code get an integer number as input and print the sum of the digits.
+
+## Input Description:
+## A single line containing an integer.
+
+## Output Description:
+## Print the sum of the digits of the integer.
+
+## Sample Input :
+## 124
+## Sample Output :
+## 7
+
+n = int(input())
+sum = 0
+for i in str(n):
+  sum += int(i)
+print(sum)
