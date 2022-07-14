dc2fcbceffdb6d9c1666b2ea0a4205f4634a71e7
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-07-14 20:02:00 +0530

    Create sum of numbers from 1 to given number.py

---
+++ absolute beginner/sum of numbers from 1 to given number.py
@@ -0,0 +1,18 @@
+## Write a code to get an integer N and print the sum of  values from 1 to N.
+
+## Input Description:
+## A single line contains an integer N.
+
+## Output Description:
+## Print the sum of values from 1 to N.
+
+## Sample Input :
+## 10
+## Sample Output :
+## 55
+
+n = int(input())
+sum = 0
+for i in range(1,n+1):
+  sum += i
+ print(sum)
