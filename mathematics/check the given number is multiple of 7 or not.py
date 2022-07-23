26d89f4d5bd705be6b3dde4f1d8e52d4a5c4383f
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-07-23 21:49:41 +0530

    Create check the given number is multiple of 7 or not.py

---
+++ mathematics/check the given number is multiple of 7 or not.py
@@ -0,0 +1,12 @@
+## Given a number N, print yes if the number is a multiple of 7 else print no.
+## Sample Testcase :
+## INPUT
+## 49
+## OUTPUT
+## yes
+
+n = int(input())
+if n%7 == 0:
+    print("yes")
+else:
+    print("no")
