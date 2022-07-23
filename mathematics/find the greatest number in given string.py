4366d3861d63c55e1fd5f20df0320824247cca37
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-07-23 21:53:44 +0530

    Update find the greatest number in given string.py

--- mathematics/find the greatest number in given string.py
+++ mathematics/find the greatest number in given string.py
@@ -1,1 +1,30 @@
+## You are given with a string which comprises of some numbers. Your task is to find the largest integer by converting the string to the corresponding integer.
 
+## Input Description:
+## First line contains n denoting number of Test Cases. The first and only Line of testcase has the string
+
+## Output Description:
+## Print the largest number
+
+## Sample Input :
+##  I was born on 12 october 1998.
+## Sample Output :
+## 1998
+
+n = input()
+n.strip()
+n = n[:(len(n)-1)]
+l = list(n.split())
+a = []
+for i in l:
+    if i[len(i)-1]==".":
+        b = i[:len(i)-1]
+    else:
+        b = i
+    try:
+        int(b)
+        a.append(int(b))
+    except:
+        "ValueError"
+a.sort()
+print(a[len(a)-1])
