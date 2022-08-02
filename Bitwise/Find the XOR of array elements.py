fb540d26e45eabcea7a4e55e1a1c59c01bce5332
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-08-02 16:51:28 +0530

    Create Find the XOR of array elements.py

---
+++ Bitwise/Find the XOR of array elements.py
@@ -0,0 +1,16 @@
+## Given a number N and an array of N elements, find the Bitwise XOR of the array elements.
+## Input Size : N <= 100000
+## Sample Testcase :
+## INPUT
+## 2
+## 2 4
+## OUTPUT
+## 6
+
+n = int(input())
+s = list(map(int,input().split()))
+sum = s[0]
+if n == len(s):
+    for i in range(1,n):
+        sum = sum^s[i]
+print(sum)
