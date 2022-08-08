e84bef2fa248c1a0dda759cd19b68ab2a8826451
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-08-08 18:57:36 +0530

    Create Bitwise and.py

---
+++ Basics/Bitwise and.py
@@ -0,0 +1,16 @@
+## Given a number N and an array of N elements ,find the Bitwise AND of the array elements.
+## Input Size N <= 100000
+## Sample Testcase :
+## INPUT
+## 4
+## 4 3 2 1
+## OUTPUT
+## 0
+
+n = int(input())
+s = list(map(int,input().split()))
+sum = s[0]
+if n == len(s):
+    for i in range(1,n):
+        sum = sum&s[i]
+print(sum)
