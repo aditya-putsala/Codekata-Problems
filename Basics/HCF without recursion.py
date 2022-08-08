74f0b2057dec7af1ee3272fd7535ec8f8df2e609
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-08-08 19:01:09 +0530

    Create HCF without recursion.py

---
+++ Basics/HCF without recursion.py
@@ -0,0 +1,23 @@
+## Write a code to get 2 integers as input and find the HCF of the 2 integer without using recursion or Euclidean algorithm.
+
+## Input Description:
+## A single line containing 2 integers separated by space.
+
+## Output Description:
+## Print the HCF of the integers.
+
+## Sample Input :
+## 2 3
+## Sample Output :
+## 1
+
+n = list(map(int,input().split()))
+a = n[0]
+b = n[1]
+if a > b:
+    a % b == 0
+    print(b)
+elif b > a:
+    b % a == 0
+    print(a)
+    
