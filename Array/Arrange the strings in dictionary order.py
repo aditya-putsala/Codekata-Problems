7d096e779624640d72d496f5067c047befaacac8
aditya-putsala <99964096+aditya-putsala@users.noreply.github.com>
2022-08-11 21:47:20 +0530

    Create Arrange the strings in dictionary order.py

---
+++ Array/Arrange the strings in dictionary order.py
@@ -0,0 +1,18 @@
+## Ria is a 5 year old girl. Her mother wants to teach her how to sort words in the same order that they appear in a dictionary. She decides to write a program to sort a given set of strings based on their alphabetical order. Help Ria’s mother to complete the program.
+
+## Input Description:
+## A set of N strings
+
+## Output Description:
+## Alphabetically sorted set of strings
+
+## Sample Input :
+## 3
+## InfinityWar EndGame Avengers
+## Sample Output :
+## Avengers EndGame InfinityWar
+
+n = input()
+k = list(input().split())
+k.sort()
+print(" ".join(k))
